6d3aabef6f0a61df4b47307581bfa13697310af3
nassim-ayadi <nassim.ayadi@medtech.tn>
2023-11-23 22:46:15 +0100

    book a meeting almostdone. need for verification

--- src/components/BookMeeting.js
+++ src/components/BookMeeting.js
@@ -1,9 +1,120 @@
-import React from 'react'
+import React, { useState } from "react";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { MultiInputTimeRangeField } from "@mui/x-date-pickers-pro";
+import Box from "@mui/material/Box";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import "./BookMeeting.css";
+import { StaticDatePicker } from "@mui/x-date-pickers/StaticDatePicker";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import Button from "@mui/material/Button";
 
 export default function BookMeeting() {
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [selectedTimeRange, setSelectedTimeRange] = useState([
+    { start: new Date(), end: new Date() },
+    { start: new Date(), end: new Date() },
+  ]);
+  const [selectedHCP, setSelectedHCP] = useState(null);
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
+  const handleTimeRangeChange = (newTimeRange) => {
+    setSelectedTimeRange(newTimeRange);
+  };
+
+  const handleHCPChange = (HCP) => {
+    setSelectedHCP(HCP);
+  };
+
+ 
+
+  const handleOk = () => {
+    // Perform actions when "OK" is clicked (e.g., save the selected date, time range, and HCP)
+    console.log("OK clicked. Selected Date:", selectedDate);
+    console.log("Selected Time Range:", selectedTimeRange);
+    console.log("Selected HCP:", selectedHCP);
+  };
+
+  const HCP = [
+    { code: "nurse", name: "monjiya", room: "376" },
+    { code: "psy", name: "mouldi ", room: "503" },
+  ];
+
   return (
-    <div>
-      book meeting 
+    <div className="page">
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <StaticDatePicker
+          orientation="landscape"
+          className="calander"
+          onChange={handleDateChange}
+          value={selectedDate}
+          slotProps={{
+
+            actionBar: {
+              actions: []
+
+          }}}
+          />
+
+        {selectedDate && (
+          <React.Fragment>
+            
+            <CardContent className="card-content">
+            
+              <Typography sx={{ fontSize: 22 }}>book your meeting</Typography>
+              <Typography sx={{ mb: 1 }} color="text.secondary">
+                set your time
+              </Typography>
+              <MultiInputTimeRangeField
+                label="Select Time Range"
+                slotProps={{
+                  textField: ({ position }) => ({
+                    label: position === "start" ? "From" : "To",
+                  }),
+                }}
+                value={selectedTimeRange}
+                onChange={handleTimeRangeChange}
+              ></MultiInputTimeRangeField>
+<div style={{display:'flex',flexDirection:'row',alignItems: 'baseline'}}>
+              <Autocomplete
+                id="HCP-select-demo"
+                sx={{ width: 250, marginTop: "20px", color: "black" }}
+                options={HCP}
+                autoHighlight
+                getOptionLabel={(option) => option.name}
+                isOptionEqualToValue={(option, value) => option.code === value.code}
+                onChange={(event, value) => handleHCPChange(value)}
+                renderOption={(props, option) => (
+                  <Box
+                    component="li"
+                    sx={{ "& > img": { mr: 2, flexShrink: 0 } }}
+                    {...props}
+                  >
+                    {option.code}: {option.name} | room:{option.room}
+                  </Box>
+                )}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    label="Choose a HCP"
+                    inputProps={{
+                      ...params.inputProps,
+                      autoComplete: "new-password", // disable autocomplete and autofill
+                    }}
+                  />
+                )}
+              />
+              <Button variant="contained" className="submit-button" onClick={handleOk}>Submit</Button>
+              </div>
+            </CardContent>
+          </React.Fragment>
+        )}
+      </LocalizationProvider>
     </div>
-  )
+  );
 }
--- src/components/ProfileDetails.js
+++ src/components/ProfileDetails.js
@@ -1,20 +1,24 @@
-import React from 'react'
-import './components/ProfileDetails.css'
-import avatar from './Assets/avatar.jpg'
+import React from "react";
+import "./ProfileDetails.css";
+import avatar from "./Assets/avatar.jpg";
 
 export default function ProfileDetails() {
   return (
     <div class="flip-card">
-  <div class="flip-card-inner">
-    <div class="flip-card-front">
-      <img src="avatar" alt="avatar" style="width:300px;height:300px;"/>
+      <div class="flip-card-inner">
+        <div class="flip-card-front">
+          <img
+            src={avatar}
+            alt="avatar"
+            style={{ width: "300px", height: "300px" }}
+          />
+        </div>
+        <div class="flip-card-back">
+          <h1>John Doe</h1>
+          <p>Architect & Engineer</p>
+          <p>We love that guy</p>
+        </div>
+      </div>
     </div>
-    <div class="flip-card-back">
-      <h1>John Doe</h1>
-      <p>Architect & Engineer</p>
-      <p>We love that guy</p>
-    </div>
-  </div>
-</div>
-  )
+  );
 }
